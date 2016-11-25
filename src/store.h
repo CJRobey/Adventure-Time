57680b4227be772706466f41cb6099204d4d81d9
agormaley <agormaley@ufl.edu>
2016-11-25 11:44:50 -0500

    Update store.h

--- src/store.h
+++ src/store.h
@@ -1,1 +1,16 @@
+#include "inventory.h" 
+#include "item.h"
 
+class Store {
+  private:
+    Inventory* inventory;
+  public:
+    void setInventory(Inventory inventory);
+    Inventory getInventory();
+    void buy(Item i);
+    void sell(Item i);
+};
+
+Store::Store(Inventory inventory) {
+ this->inventory = new Inventory(inventory);
+}
