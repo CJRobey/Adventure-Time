64e1ce35e7859e9b57c9a3583a4a8ba21c6e7fcf
agormaley <agormaley@ufl.edu>
2016-11-25 10:56:49 -0500

    Update inventory.h

--- src/inventory.h
+++ src/inventory.h
@@ -1,1 +1,33 @@
 
+class Item {
+  private:
+    String type;
+    int cost;
+  public:
+    void setType(String type);
+    void setCost(int cost);
+    String getType();
+    int getCost();
+     
+};
+
+Item::Item(String type, int cost) {
+  this->type = type;
+  this->cost = cost;
+}
+
+void setType(String type){
+  this->type = type;
+}
+
+void setCost(int cost){
+  this->cost = cost;
+}
+
+String getType(){
+  return this->type;
+}
+
+int getCost(){
+  return this->cost;
+}
