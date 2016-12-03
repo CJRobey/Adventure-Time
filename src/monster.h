7790b86a0fbc047af9d69ae7b42db126a03686c1
agormaley <agormaley@ufl.edu>
2016-12-03 09:59:23 -0500

    Update monster.h

--- src/monster.h
+++ src/monster.h
@@ -8,9 +8,10 @@ class Monster {
   public:
     int getAttack();
     int getHealth();
-    int isAttacked();
     void setAttack();
     void setHealth();
+    void decHealth(int attackPower);
+    void ptak();
 };
 
 Monster::Monster(int level)
@@ -26,6 +27,11 @@ int getHealth(){
   return health;
 }
 
-void isAttacked(){
+void decHealth(int attackPower){
+  
   //print updated monster stats--> if dead make a big deal;
+}
+
+void ptak(Character* char){
+ //print character points
 }
