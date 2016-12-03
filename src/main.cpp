dda938507e782da69c632669e1a9e3d23453dbcf
snl1101 <snl1101@verizon.net>
2016-12-03 16:57:54 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ int main () {
 			cout << plot.text[0] << plot.text[1];
        			cin >> pChoice;
 
-        		while(pChoice != 1 || 3){
+        		while(pChoice != 1 && pChoice != 3){
 
             		cout << plot.text[2];
            		cin >> pChoice;
@@ -246,7 +246,168 @@ int main () {
 	}
 //////////////////////////////////////////////////////////////////////////
 		case (3): {
-			
+			pChoice = 0;
+        cout << plot.text[54];
+        while(pChoice != 1 || 2){
+            cout << plot.text[55];
+            cin >> pChoice;
+
+            if(pChoice == 1){
+
+            }
+            if(pChoice == 2){
+                cout << plot.text[57];
+                cin >> pChoice;
+                if(pChoice == 1){
+                    cout << plot.text[59];
+                    cin >> pChoice;
+                    if(pChoice == 1){
+                        if(chance(50) == true){
+                            cout << plot.text[60];
+                        }
+                        if(chance(50) == false){
+                            cout << plot.text[61];
+                            //-5% health
+                        }
+                    }
+                    if(pChoice == 2){
+                        cout << plot.text[62];
+                    }
+                }
+                if(pChoice == 2){
+                    cout << plot.text[58];
+                }
+
+            }
+            if(pChoice == 3){
+                cout << plot.text[56];
+                found == true;
+            }
+            if(pChoice == 3 && found == true){
+                cout << "You already investigated the bakery. Choose something else.";
+            }
+        }
+
+        cout << plot.text[63];
+        cin >> pChoice;
+
+        if(pChoice == 1){
+            cout << plot.text[64];
+        }
+        if(pChoice == 2){
+            cout << plot.text[65];
+        }
+
+        cout << plot.text[66] << plot.text[67] << plot.text[68];
+        cout << plot.text[69];
+        cin >> pChoice;
+        if(pChoice == 1){
+            cout << plot.text[70];
+        }
+        if(pChoice == 2){
+            cout << plot.text[71];
+        }
+        if(pChoice == 3){
+            cout << plot.text[72];
+
+            cout << plot.text[73];
+            cin >> pChoice;
+
+            //Choices 1 & 2 lead to same point
+            if(pChoice == 1 || pChoice == 2){
+            if(pChoice == 1){
+                cout << plot.text[75];
+                //character.battle(thieves)
+            }
+            if(pChoice == 2){
+                cout << plot.text[76];
+                cin >> pChoice;
+                if(pChoice == 1){
+                    //character.battle(thieves) - surprise attack
+                }
+                if(pChoice == 2){
+                    cout << plot.text[77];
+                    //character.battle(thieves)
+                }
+            }
+            pChoice = 0;
+            cout << plot.text[78];
+            while(pChoice != 3){
+                cout << plot.text[79];
+                cin >> pChoice;
+                if(pChoice == 1){
+                    if(leftRoom == true){
+                        cout << "You have already been here. Choose another option."
+                    }
+                    else{
+                        cout << plot.text[80];
+                        Character.incGold(400);
+                        leftRoom = true;
+                    }
+                }
+                if(pChoice == 2){
+                    if(rightRoom == true){
+                        cout << "You have already been here. Choose another option."
+                    }
+                    else{
+                        cout << plot.text[81];
+                        rightRoom = true;
+                    }
+                }
+                if(pChoice == 3){
+                    cout << plot.text[82];
+                }
+            }
+            }
+            if(pChoice == 3){
+                cout << plot.text[74];
+            }
+        }
+        cout << plot.text[83];
+        pChoice = 0;
+        while(pChoice != 3){
+            cout << plot.text[84];
+            cin >> pChoice;
+
+            if(pChoice == 1){
+                if(seenBeggar == true){
+                    cout << "You've already talked with the beggar. Choose another option.";
+                }
+                else{
+                    int beggarValue = 0;
+                    cout << plot.text[85];
+                    cin >> beggarValue;
+                    if(beggarValue == 0){
+                        cout << "\"Ok. Thank you for your time anyway.\"";
+                    }
+                    else if(beggarValue == 27){
+                        //CONSPIRACY
+                    }
+                    else{
+                        cout << "\"Thank you so much. I wish you the best of luck in all that you do.\"";
+                    }
+                    seenBeggar = true;
+                }
+            }
+            if(pChoice == 2){
+                cout << plot.text[86];
+                pChoice = 0;
+                while(pChoice != 2){
+                    cout << plot.text[87];
+                    cin >> pChoice;
+                    if(pChoice == 1){
+                        //open store
+                    }
+                    if(pChoice == 2){
+                        cout << plot.text[88];
+                    }
+                }
+            }
+            if(pChoice == 3){
+                cout << plot.text[89];
+            }
+        }
+
 		}
 		case (4): {
 		
