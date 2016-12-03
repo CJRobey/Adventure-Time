bdf4927060ceffea130610c88d62d6240e3295fd
agormaley <agormaley@ufl.edu>
2016-12-03 13:16:04 -0500

    Update item.h

--- src/item.h
+++ src/item.h
@@ -1,38 +1,3 @@
-
-class Item {
-  private:
-    String type;
-    int cost;
-  public:
-    Item(String type, int cost);
-    void setType(String type);
-    void setCost(int cost);
-    String getType();
-    int getCost();
-     
-};
-
-Item::Item(String type, int cost) {
-  this->type = type;
-  this->cost = cost;
-}
-
-void setType(String type){
-  this->type = type;
-}
-
-void setCost(int cost){
-  this->cost = cost;
-}
-
-String getType(){
-  return this->type;
-}
-
-int getCost(){
-  return this->cost;
-}
-=======
 #include <iostream>
 #include <string>
 
