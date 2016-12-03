bb553545f2d467b3175f6df2af6ba2a800b204f4
snl1101 <snl1101@verizon.net>
2016-12-03 13:10:33 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,145 @@ int main () {
 	string pResponse = "";
 	Store s;
 	
+	bool chance(int luck) {
+ 		int v1 = rand() * 100;
+  		if (v1 < luck) {
+    			return true;
+  		}
+  		else return false;
+		}
+	
 	switch (save) {
 		Character hero = new Character;
 		
 		case (1): {
-			
+			cout << plot.text[0] << plot.text[1];
+       			cin >> pChoice;
+
+        		while(pChoice != 1 || 3){
+
+            		cout << plot.text[2];
+           		cin >> pChoice;
+
+            		if(pChoice == 1){
+                		cout << plot.text[3];
+                		Character.incGold(500);
+            		}
+            if(pChoice == 2){
+                cout << plot.text[4];
+            }
+            if(pChoice == 3){
+
+                if (chance(50) == true){
+                    cout << plot.text[5];
+                    Character.incGold(750);
+                }
+                if (chance(50) == false){
+                    cout << plot.text[6]
+                    Character.incGold(500);
+                }
+            }
+            else{
+                cout << "Please enter an option from the menu."
+            }
+        }
+
+        cout << plot.text[7];
+        while (pChoice != 2){
+
+            cout.text[8];
+            cin >> pChoice;
+
+            if(pChoice == 1){
+                cout << plot.text[9];
+                Character.decGold(1);
+                }
+            if(pChoice == 2){
+                cout << plot.text[10];
+                cin >> pChoice;
+                if(pChoice == 1){
+                    cout << plot.text[11];
+                    character.incGold(50);
+                }
+                if(pChoice == 2){
+                    cout << plot.text[12];
+                }
+               cout << plot.text[13];
+               //open and close store
+               cout << plot.text[14];
+            }
+            else{
+                cout << "Please enter an option from the menu.";
+            }
+        }
+
+        cout << plot.text[15];
+        cout << plot.text[16];
+        cin >> pChoice;
+
+        if(pChoice == 1){
+            //character.battle(wolves)
+            //helped++;
+            //level up
+        }
+        if(pChoice == 2){
+        }
+
+        cout << plot.text[17];
+        cout << plot.text[18];
+        cin >> pChoice;
+
+        if(pChoice == 1){
+            cout << plot.text [19];
+            character.incGold(20);
+            cout << plot.text[20];
+            cin >> pChoice;
+
+            if(pChoice == 1){
+                cout << plot.text[22];
+                cout << plot.text[23];
+                cin >> pChoice;
+
+                if(pChoice == 1){
+                    //character.battle(bandits)
+                    cout << plot.text[24];
+                    Character.incGold(150);
+                    helped++;
+                    //level up
+                }
+                if(pChoice == 2){
+                    if(chance(40) == true){
+                        //character.battle(bandits)
+                        cout << plot.text[24];
+                        Character.incGold(150);
+                        helped++;
+                        //level up
+                    }
+                    if(chance(40) == false){
+                        //character.battle(bandits)
+                        helped++;
+                    }
+                }
+                if(pChoice == 3){
+                    if(chance(60) == true){
+                        cout << plot.text[25];
+                    }
+                    if(chance(60) == false){
+                        //character.battle(bandits)
+                        cout << plot.text[24];
+                        helped++;
+                    }
+                }
+
+            }
+
+            if(pChoice == 2){
+                cout << plot.text[21];
+            }
+        }
+        cout << plot.text[26];
 		}
+			
 		case (2): {
 			cout<<plot.text[27];
 			cin>>pChoice;
